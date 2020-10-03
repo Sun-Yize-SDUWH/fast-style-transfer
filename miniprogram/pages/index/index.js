3171e0f6a6ff463ef731449d1b7aa2bcd9e2897e
Charlie Sun <986765210@qq.com>
2020-10-04 01:47:36 +0800

    AI画家1.0 final

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,4 +1,3 @@
-"use strict";
 Page({
   data: {
     title: "ddemo",
@@ -12,93 +11,8 @@ Page({
       out: ""
     }]
   },
-  touchStart: function (t) {
-    this.setData({
-      startX: t.changedTouches[0].clientX
-    })
-  },
-  touchEnd: function (t) {
-    var e = this,
-      n = this.data.view;
-    this.setData({
-      endX: t.changedTouches[0].clientX
-    });
-    var a = t.changedTouches[0].clientX - this.data.startX;
-    if (a < -100) {
-      if (this.data.currentIndex >= 1) return;
-      this.setData({
-        oldIndex: e.data.currentIndex,
-        currentIndex: ++e.data.currentIndex
-      }), n[this.data.oldIndex].out = "animated fadeOutLeft", n[this.data.oldIndex].in = "", n[this.data.currentIndex].in = "animated fadeInRight", n[this.data.currentIndex].out = "", this.setData({
-        view: n
-      }), this.cleanAnimated(), this.showAnimated()
-    } else if (a > 100) {
-      if (this.data.currentIndex <= 0) return;
-      this.setData({
-        oldIndex: e.data.currentIndex,
-        currentIndex: --e.data.currentIndex
-      }), n[this.data.oldIndex].out = "animated fadeOutRight", n[this.data.oldIndex].in = "", n[this.data.currentIndex].in = "animated fadeInLeft", n[this.data.currentIndex].out = "", this.setData({
-        view: n
-      }), this.cleanAnimated(), this.showAnimated()
-    }
-  },
-  showAnimated: function () {
-    var t = this;
-    0 === this.data.currentIndex ? (setTimeout(function () {
-      t.setData({
-        one_one: "animated fadeIn",
-        one_two: "animated bounceIn"
-      })
-    }, 1e3), setTimeout(function () {
-      t.setData({
-        one_three: "animated bounceIn"
-      })
-    }, 150), setTimeout(function () {
-      t.setData({
-        one_four: "animated bounceIn"
-      })
-    }, 180), setTimeout(function () {
-      t.setData({
-        one_five: "animated lightSpeedIn"
-      })
-    }, 190)) : 1 === this.data.currentIndex && (setTimeout(function () {
-      t.setData({
-        two_one: "animated fadeInDown",
-        two_two: "animated fadeInUp"
-      })
-    }, 1e3), setTimeout(function () {
-      t.setData({
-        two_three: "animated zoomIn",
-        two_four: "animated zoomIn"
-      })
-    }, 120), setTimeout(function () {
-      t.setData({
-        two_three: "two-music-one",
-        two_four: "two-music-two"
-      })
-    }, 220))
-  },
-  cleanAnimated: function () {
-    0 === this.data.oldIndex ? this.setData({
-      one_one: "animated fadeOut",
-      one_two: "animated fadeOut",
-      one_three: "animated fadeOut",
-      one_four: "animated fadeOut",
-      one_five: "animated fadeOut"
-    }) : 1 === this.data.oldIndex && this.setData({
-      two_one: "animated fadeOut",
-      two_two: "animated fadeOut",
-      two_three: "animated fadeOut",
-      two_four: "animated fadeOut",
-      two_five: "animated fadeOut"
-    })
-  },
+
   onLoad: function (t) {
-   
-  },
-  onReady: function () {},
-  onShow: function (t) {
-    
     this.showAnimated();
     var t = this;
     setTimeout(function () {
@@ -139,31 +53,117 @@ Page({
       })
     }, 340)
   },
+
   onHide: function () {
-    this.cleanAnimated(), this.setData({
-      bottom: "",
-      bottom_one: "",
-      bottom_two: "",
-      bottom_three: "",
-      bottom_four: ""
+    // this.cleanAnimated(), this.setData({
+    //   bottom: "",
+    //   bottom_one: "",
+    //   bottom_two: "",
+    //   bottom_three: "",
+    //   bottom_four: ""
+    // })
+  },
+
+  touchStart: function (t) {
+    this.setData({
+      startX: t.changedTouches[0].clientX
     })
   },
-  navigate1:function(){
+
+  touchEnd: function (t) {
+    var e = this,
+      n = this.data.view;
+    this.setData({
+      endX: t.changedTouches[0].clientX
+    });
+    var a = t.changedTouches[0].clientX - this.data.startX;
+    if (a < -100) {
+      if (this.data.currentIndex >= 1) return;
+      this.setData({
+        oldIndex: e.data.currentIndex,
+        currentIndex: ++e.data.currentIndex
+      }), n[this.data.oldIndex].out = "animated fadeOutLeft", n[this.data.oldIndex].in = "", n[this.data.currentIndex].in = "animated fadeInRight", n[this.data.currentIndex].out = "", this.setData({
+        view: n
+      }), this.cleanAnimated(), this.showAnimated()
+    } else if (a > 100) {
+      if (this.data.currentIndex <= 0) return;
+      this.setData({
+        oldIndex: e.data.currentIndex,
+        currentIndex: --e.data.currentIndex
+      }), n[this.data.oldIndex].out = "animated fadeOutRight", n[this.data.oldIndex].in = "", n[this.data.currentIndex].in = "animated fadeInLeft", n[this.data.currentIndex].out = "", this.setData({
+        view: n
+      }), this.cleanAnimated(), this.showAnimated()
+    }
+  },
+
+  showAnimated: function () {
+    var t = this;
+    0 === this.data.currentIndex ? (setTimeout(function () {
+      t.setData({
+        one_one: "animated fadeIn",
+        one_two: "animated bounceIn"
+      })
+    }, 1e3), setTimeout(function () {
+      t.setData({
+        one_three: "animated bounceIn"
+      })
+    }, 150), setTimeout(function () {
+      t.setData({
+        one_four: "animated bounceIn"
+      })
+    }, 180), setTimeout(function () {
+      t.setData({
+        one_five: "animated lightSpeedIn"
+      })
+    }, 190)) : 1 === this.data.currentIndex && (setTimeout(function () {
+      t.setData({
+        two_one: "animated fadeInDown",
+        two_two: "animated fadeInUp"
+      })
+    }, 1e3), setTimeout(function () {
+      t.setData({
+        two_three: "animated zoomIn",
+        two_four: "animated zoomIn"
+      })
+    }, 120), setTimeout(function () {
+      t.setData({
+        two_three: "two-music-one",
+        two_four: "two-music-two"
+      })
+    }, 220))
+  },
+
+  cleanAnimated: function () {
+    0 === this.data.oldIndex ? this.setData({
+      one_one: "animated fadeOut",
+      one_two: "animated fadeOut",
+      one_three: "animated fadeOut",
+      one_four: "animated fadeOut",
+      one_five: "animated fadeOut"
+    }) : 1 === this.data.oldIndex && this.setData({
+      two_one: "animated fadeOut",
+      two_two: "animated fadeOut",
+      two_three: "animated fadeOut",
+      two_four: "animated fadeOut",
+      two_five: "animated fadeOut"
+    })
+  },
+
+  navigate1: function () {
     wx.navigateTo({
-      url: '../mine/mine',
+      url: '/pages/main/main',
     })
   },
-  navigate2:function(){
+
+  navigate2: function () {
     wx.navigateTo({
-      url: '../mine/mine',
+      url: '/pages/introduce/introduce',
     })
   },
-  navigate3:function(){
+
+  navigate3: function () {
     wx.navigateTo({
-      url: '../mine/mine',
+      url: '/pages/mine/mine',
     })
-  },
-  onUnload: function () {},
-  onPullDownRefresh: function () {}
-});
-//# sourceMappingURL=ddemo.js.map
+  }
+})
