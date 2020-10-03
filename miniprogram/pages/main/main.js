4bc856366b0f8881d7a85c295a0d6831b77949f0
Xu-Hanjia-SDUWH <ralphxu815@163.com>
2020-10-03 22:38:32 +0800

    10.3 final

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -1,69 +1,111 @@
-// pages/main1/main1.js
 Page({
-
+  /**
+   * 页面的初始数据
+   */
   data: {
-    currentValue: 50,
+    show: false,
+    text:'\n\n风格简介',
+    list: [
+      {num:1,name:'风格1',proUrl:'../../images/11.png',des:"hhh1"},
+      {num:2,name:'风格2',proUrl:'../../images/12.png',des:"hhh2"},
+      {num:3,name:'风格3',proUrl: '../../images/5.png',des:"hhh3"},
+      {num:4,name:'风格4',proUrl:'../../images/6.jpg',des:"hhh4"}, 
+      {num:5,name:'风格5',proUrl:'../../images/1.png',des:"hhh5"},
+      {num:6,name:'风格6',proUrl:'../../images/1.png',des:"hhh6"},
+      {num:7,name:'风格7',proUrl:'../../images/1.png',des:"hhh7"}
+    ],
+    current: 0,
+    animationData: {},
+    animationData2: {}
   },
-
-  onDrag(event) {
-    this.setData({
-      currentValue: event.detail.value,
-    });
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.stretch(350)
+  },
+  change(e){
+    this.setData({
+      current: e.detail.current
+    })
+    this.stretch(350)
+    
+    this.shrink(300)
+  },
+  // 收缩
+  stretch(h){
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+    this.animation = animation
+    animation.height(h).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+  },
+  // 展开
+  shrink(h){
+    var animation2 = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+    this.animation2 = animation2
+    animation2.height(h).step()
+    this.setData({
+      animationData2: animation2.export()
+    })
+  },
+  showPopup() {
+    this.setData({ show: true });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onClose() {
+    this.setData({ show: false });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
+
+  goToDescription1(){
+
+    wx.navigateTo({
+      url: '../pic1/pic1',
+    })
   },
+  goToDescription2(){
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+    wx.navigateTo({
+      url: '../pic2/pic2',
+    })
   },
+  goToDescription3(){
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    wx.navigateTo({
+      url: '../pic3/pic3',
+    })
   },
+  goToDescription4(){
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+    wx.navigateTo({
+      url: '../pic4/pic4',
+    })
   },
+  goToDescription5(){
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+    wx.navigateTo({
+      url: '../pic5/pic5',
+    })
   },
+  goToDescription6(){
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    wx.navigateTo({
+      url: '../pic6/pic6',
+    })
+  },
+  goToDescription7(){
 
+    wx.navigateTo({
+      url: '../pic7/pic7',
+    })
   }
 })
